f725a4bbe17f607e8e3cf051b3b5c8f416705843
Eduardo Trujillo <eduardo.work.world@gmail.com>
2025-05-17 20:15:17 +0100

    COnfigured dockekr compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtsract(x, y):
+    """ substrack x from y and return result."""
+    return y - x
